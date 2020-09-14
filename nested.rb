8da1352ba44921dddcb3bbab41b12d66c8f9fca4
Brock Byrd <brockbyrdd@gmail.com>
2020-09-14 19:18:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,12 +82,6 @@ def adding_matz
         }
      }
      
-     new_programmer = [:yukihiro_matsumoto] => 
-      { 
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-      }
-     programmer_hash.
 end
 
 def changing_alan
